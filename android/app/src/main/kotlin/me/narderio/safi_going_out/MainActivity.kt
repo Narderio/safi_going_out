7cbdb9d91d134dfce806284cffe25b09dcc914d0
Dario Nardella <80456732+Narderio@users.noreply.github.com>
2025-02-18 18:54:51 +0100

    create packages front-end and back-end

--- android/app/src/main/kotlin/me/narderio/safi_going_out/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package me.narderio.safi_going_out
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:safi_going_out/safi_going_out.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-
-
